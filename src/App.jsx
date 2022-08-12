fea38e85e8221184d47b74085b2fd0f6e0498a4b
dev_geampiere <jeanpiere.virgilio@gmail.com>
2022-08-12 13:33:59 -0500

    Creando el formulario presupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import Header from './components/Header'
 import { useState } from 'react'
 
 function App() {
-
+  const [presupuesto, setPresupuesto] = useState(0)
   return (
     <div>
-      <Header />
+      <Header 
+        presupuesto = {presupuesto}
+        setPresupuesto = {setPresupuesto}
+      />
     </div>
   )
 }
