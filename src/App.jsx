2acf83327571e8612599c1d08213920fc7178b8c
dev-geampiere <jeanpiere.virgilio@gmail.com>
2022-11-16 10:12:20 -0500

    Editando los gastos

--- src/App.jsx
+++ src/App.jsx
@@ -35,10 +35,17 @@ function App() {
         }, 500);
     }
     const guardarGasto = gasto => {
-        gasto.id = generarId();
-        gasto.fecha = Date.now();
-        setGastos([...gastos, gasto]);
-
+        if (gasto.id) {
+            // Actualizar
+            const gastosActualizados = gastos.map( gastoState => gastoState.id === gasto.id ? gasto : gastoState )
+            setGastos(gastosActualizados)
+            
+        } else {
+            // Nuevo Gasto
+            gasto.id = generarId();
+            gasto.fecha = Date.now();
+            setGastos([...gastos, gasto]);
+        }
         setAnimarModal(false)
         setTimeout(() => {
             setModal(false)
