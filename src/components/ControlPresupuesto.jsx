39ac7b308d1f83a3ed303d82572f983e44db8801
dev-geampiere <jeanpiere.virgilio@gmail.com>
2022-12-05 15:02:46 -0500

    Agregando grafica y calculando el porcentaje

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from 'react'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
 const ControlPresupuesto = ({gastos, presupuesto}) => {
 
+    const [porcentaje, setPorcentaje] = useState(0)
     const [disponible, setDisponible] = useState(0)
     const [gastado, setGastado] = useState(0)
 
     useEffect(() => {
         const totalGastado = gastos.reduce( (total, gasto) => gasto.cantidad + total, 0);
-
         const totalDisponible = presupuesto - totalGastado
+
+        // Calcular el porcentaje gastado
+        const nuevoPorcentaje = (((presupuesto - totalDisponible) / presupuesto) * 100).toFixed(2)
 
         setDisponible(totalDisponible)
         setGastado(totalGastado)
+        setTimeout(() => {
+            setPorcentaje(nuevoPorcentaje)
+        }, 1500);
     }, [gastos])
     
 
@@ -26,7 +34,9 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
     return ( 
         <div className="contenedor-presupuesto contenedor sombra dos-columnas">
             <div>
-                <p>Grafica aqui</p>
+                <CircularProgressbar
+                    value={porcentaje}
+                />
             </div>
             <div className="contenido-presupuesto">
                 <p>
